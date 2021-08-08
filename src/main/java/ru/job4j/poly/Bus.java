df6102204655d37f163631c1c945ec8671daadc7
Tolik-NewEl <tsepushtanov@outlook.com>
2021-08-08 12:10:47 +0500

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void pass(int pass) {
+
+    }
+
+    @Override
+    public double oil(double price) {
+        return 0;
+    }
+}
