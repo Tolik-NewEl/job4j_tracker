20231629dba459e19c9795404eef20d3a3d709ff
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2021-09-27 15:43:41 +0500

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                if (start.equals("")) {
+                    start = el;
+                } else {
+                    start += "/" + el;
+                }
+                tmp.add(start);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static List<String> sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+        return orgs;
+    }
+
+    public static List<String> sortDesc(List<String> orgs) {
+        Collections.sort(orgs, new DepDescComp());
+        return orgs;
+    }
+}
