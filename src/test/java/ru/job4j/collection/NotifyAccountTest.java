9b2b4c5252b1d61330ba5bdc1f53c995e5dd81dc
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2021-10-02 13:37:19 +0500

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -2,7 +2,6 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,13 +12,13 @@ public class NotifyAccountTest {
 
     @Test
     public void whenDeleteDuplicate() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"),
                 new Account("123", "Petr Arsentev", "eDer3432f")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
@@ -29,12 +28,12 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
